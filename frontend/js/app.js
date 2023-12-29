707510dfa79823c84e523e5c45176c1ce1e5efc7
Manar Ben Salah <manarbenssssalah@gmail.com>
2023-12-29 17:27:48 +0100

    version final

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,80 +1,84 @@
-var BTN=document.querySelector("#bText")
-var TEXTAREA=document.querySelector("#textSpeech")
-var DIV=document.querySelector("#reponse_msg")
-var BTN_MIC=document.querySelector("#bMic")
-var BTN_SP=document.querySelector("#bSpeak")
+var BTN = document.querySelector("#bText");
+var TEXTAREA = document.querySelector("#textSpeech");
+var DIV = document.querySelector("#response_msg");
+
+var BTN_MIC = document.querySelector("#bMic");
+var BTN_SP = document.querySelector("#bSpeak");
 
 var recognition = new webkitSpeechRecognition();
 recognition.continuous = false;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
-//EVENEMENT
-BTN.addEventListener("click", chatBot)
-BTN_MIC.addEventListener("click", speechToText)
-//fonction principale
-function chatBot(){
-    let text=TEXTAREA.value
-    //je dois communiquer avec le backend
-    var url_backend="http://127.0.0.1:8001/analyse"
-    fetch(url_backend,
-        {
-            method:"POST",
-            body:JSON.stringify({"texte":text}),
-            headers:{  
-                'Content-Type': 'application/json'
-            }          
-        })
-    .then(reponse=>{
+
+// Declare data in a higher scope
+var data;
+
+// Use Web Speech API for text-to-speech
+BTN_SP.addEventListener("click", function (event) {
+    event.preventDefault();
+    // Check if data is defined before calling startTextToSpeech
+    if (data) {
+        startTextToSpeech(data.msg);
+    }
+});
+
+// EVENEMENT
+BTN.addEventListener("click", chatBot);
+BTN_MIC.addEventListener("click", speechToText);
+
+// fonction principale
+function chatBot() {
+    let text = TEXTAREA.value;
+
+    // je dois communiquer avec le backend
+    var url_backend = "http://127.0.0.1:8001/analyse";
+    fetch(url_backend, {
+        method: "POST",
+        body: JSON.stringify({ "texte": text }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(reponse => {
         reponse.json()
-        .then(data=>{
-            BTN_SP.style.display=""
-            //this bit makes the sound play directly without waiting for the button to be clicked on
-            // BTN_SP.addEventListener("click",texteToSpeech(data.msg)) 
-            //this only plays when the button is clicked on
-            BTN_SP.addEventListener("click", function() {
-                texteToSpeech(data.msg);
-            });
-            
-            console.log(data.msg)
-            // DIV.innerHTML=data.msg
-            var i=0;
-            var txt=data.msg;
-            var speed=50;
+            .then(receivedData => {
+                data = receivedData; // Assign received data to the outer variable
+                BTN_SP.style.display = "";
 
-            function typeWriter()
-            { if (i<txt.length)
-                { DIV.innerHTML+=txt.charAt(i);
-                  i++;
-                  setTimeout(typeWriter,speed);
-                }
-            }
-            typeWriter()
-        })
+                BTN_SP.addEventListener("click", function () {
+                    texteToSpeech(data.msg);
+                });
+
+                console.log(data.msg);
+                // Format the bot's response in a green rectangle
+                var botMessageHTML = `<div class="message bot-message">${data.msg}</div>`;
+                DIV.innerHTML += botMessageHTML;
+            })
     })
-    .catch(e=>{
-        console.warn(e)
-    })
-}
-function speechToText(){
-    // alert("Je suis speech to text")
-    //1ère partie déclencher l'API Speech To Text
-     recognition.start();
-
-}
-function texteToSpeech(texte)
-{let utterance = new SpeechSynthesisUtterance(texte);
-  speechSynthesis.speak(utterance);
-
+    .catch(e => {
+        console.warn(e);
+    });
 }
 
-recognition.onresult = function(event) {
+function speechToText() {
+    recognition.start();
+}
 
-    //2ème partie récupérer le texte
+function startTextToSpeech(texte) {
+    console.log('startTextToSpeech called with text:', texte);
+    const speechOutput = new SpeechSynthesisUtterance();
+    speechOutput.text = texte;
+
+    // Set language to English
+    speechOutput.lang = 'en-US'; // or 'en'
+
+    speechSynthesis.speak(speechOutput);
+}
+
+recognition.onresult = function (event) {
     var message = event.results[0][0].transcript;
     console.log('Result received: ' + message + '.');
-     console.log('Confidence: ' + event.results[0][0].confidence);
-
-    //3ème partie remplir l'input en utilisant ce texte
-    TEXTAREA.value=message
-  }
+    console.log('Confidence: ' + event.results[0][0].confidence);
+    TEXTAREA.value = message;
+}
